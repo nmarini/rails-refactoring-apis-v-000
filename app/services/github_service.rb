d80f79de5760a4f7e62a9fa03446c611b89405b1
Nicolas Marini <nmarini44@gmail.com>
2019-06-14 23:27:57 +0000

    Automatically backed up by Learn

--- app/services/github_service.rb
+++ app/services/github_service.rb
@@ -3,4 +3,10 @@ attr_reader :access_token
   def initialize(hash = nil)
     @access_token = hash["access_token"] if hash
   end
+
+  def authenticate!(client_id, client_secret, code)
+    response = Faraday.post "https://github.com/login/oauth/access_token", {client_id: client_id, client_secret: client_secret,code: code}, {'Accept' => 'application/json'}
+    access_hash = JSON.parse(response.body)
+    session[:token] = access_hash["access_token"]
+  end 
 end
